0590f956569bc266fbbbaefbd10b60473802b91a
joshz123 <joshzwiebel@gmail.com>
2018-11-22 18:11:37 -0500

    i

---
+++ cmake-build-debug/CMakeFiles/rgbfindergood.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rgbfindergood.dir/main.cpp.o"
+  "rgbfindergood.pdb"
+  "rgbfindergood.exe"
+  "librgbfindergood.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rgbfindergood.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
